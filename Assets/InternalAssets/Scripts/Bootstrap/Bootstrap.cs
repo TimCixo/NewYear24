e08a6707004190f1e3ea52c35e8955b76a356502
TimCixo <enderhiplol@gmail.com>
2024-12-29 12:21:55 +0200

    Update: Enhance Bootstrap class to support conditional initialization with IBootstrapable interface

--- Assets/InternalAssets/Scripts/Bootstrap/Bootstrap.cs
+++ Assets/InternalAssets/Scripts/Bootstrap/Bootstrap.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 
-public class Bootstrap : MonoBehaviour
+public class Bootstrap : MonoBehaviour, IBootstrapable
 {
+    [SerializeField]
+    private bool _isBootstrapable;
     [SerializeField]
     private MonoBehaviour[] _components;
 
     private void Awake()
     {
+        if (_isBootstrapable)
+        {
+            return;
+        }
+
         BootstrapInit();
     }
 
-    private void BootstrapInit()
+    public void BootstrapInit()
     {
         foreach (MonoBehaviour component in _components)
         {
